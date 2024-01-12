285e64d5487371285b349ebfbd9f040e1c3dc46b
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2024-01-12 08:45:53 -0600

    Add functionality to the 2 new channels

--- src/main/java/me/justahuman/slimefun_server_essentials/channels/ItemChannel.java
+++ src/main/java/me/justahuman/slimefun_server_essentials/channels/ItemChannel.java
@@ -1,8 +1,24 @@
 package me.justahuman.slimefun_server_essentials.channels;
 
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
+import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
+import org.bukkit.entity.Player;
+
+import javax.annotation.Nonnull;
+
 public class ItemChannel extends AbstractChannel {
     @Override
     public String getChannel() {
         return "slimefun_server_essentials:item";
+    }
+
+    @Override
+    public void onRegisterConnection(@Nonnull Player player) {
+        final List<SlimefunItem> slimefunItems = Slimefun.getRegistry().getAllSlimefunItems();
+        final List<SlimefunItem> enabledItems = Slimefun.getRegistry().getEnabledSlimefunItems();
+        slimefunItems.removeAll(enabledItems);
+        for (SlimefunItem slimefunItem : slimefunItems) {
+            sendMessage(player, slimefunItem.getId());
+        }
     }
 }
--- src/main/java/me/justahuman/slimefun_server_essentials/channels/ItemModelsChannel.java
+++ src/main/java/me/justahuman/slimefun_server_essentials/channels/ItemModelsChannel.java
@@ -1,8 +1,32 @@
 package me.justahuman.slimefun_server_essentials.channels;
 
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
+import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
+import org.bukkit.entity.Player;
+
+import javax.annotation.Nonnull;
+
 public class ItemModelsChannel extends AbstractChannel {
     @Override
     public String getChannel() {
         return "slimefun_server_essentials:item_models";
+    }
+
+    @Override
+    public void onRegisterConnection(@Nonnull Player player) {
+        for (SlimefunItem slimefunItem : Slimefun.getRegistry().getEnabledSlimefunItems()) {
+            final String id = slimefunItem.getId();
+            final int model = Slimefun.getItemTextureService().getModelData(id);
+            if (model == 0) {
+                continue;
+            }
+
+            final ByteArrayDataOutput packet = ByteStreams.newDataOutput();
+            packet.writeInt(model);
+            packet.writeUTF(id);
+            sendMessage(player, packet);
+        }
     }
 }
